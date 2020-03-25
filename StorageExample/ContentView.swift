6fbbc86674e40ef172043b9878fd4890cb758c5e
merlox <merunasgrincalaitis@gmail.com>
2020-03-25 15:55:43 +0100

    Initial Commit

---
+++ StorageExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StorageExample
+//
+//  Created by Merunas Grincalaitis on 25/03/2020.
+//  Copyright © 2020 Merunas Grincalaitis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
